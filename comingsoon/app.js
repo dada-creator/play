061a3e976fc02f3288d2e425d0864fbfec38547c
dada-creator <71347219+dada-creator@users.noreply.github.com>
2021-05-12 20:49:00 +0200

    added confetis in coming soon

--- comingsoon/app.js
+++ comingsoon/app.js
@@ -1,22 +1,38 @@
+import { startconfeti } from './confetti.js';
+
 const countdown = () => {
-    const countDate = new Date('May 15, 2021 00:00:00').getTime();
+    const countDate = new Date('May 12, 2021 20:48:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
+    
+    if (gap > 0) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
-
-    document.querySelector('.day').innerText = textDay;
-    document.querySelector('.hour').innerText = textHour;
-    document.querySelector('.minute').innerText = textMinute;
-    document.querySelector('.second').innerText = textSecond;
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) / hour);
+        const textMinute = Math.floor((gap % hour) / minute);
+        const textSecond = Math.floor((gap % minute) / second);
+        
+        document.querySelector('.day').innerText = textDay;
+        document.querySelector('.hour').innerText = textHour;
+        document.querySelector('.minute').innerText = textMinute;
+        document.querySelector('.second').innerText = textSecond;
+    } else {
+        document.querySelector('.day').innerText = "0";
+        document.querySelector('.hour').innerText = "0";
+        document.querySelector('.minute').innerText = "0";
+        document.querySelector('.second').innerText = "0";
+        
+        startconfeti()
+        console.log("confeti started")
+        clearInterval(refreshIntervalId);
+        //window.location.reload(true)
+        
+        //document.write('<canvas id="canvas" style="position: absolute; bottom: 0; left: 0; width: 100%;"></canvas>');
+    }
 }
-countdown()
-setInterval(countdown, 1000)
+//countdown()
+var refreshIntervalId = setInterval(countdown, 1000);
--- redirect.js
+++ redirect.js
@@ -1,1 +1,7 @@
-window.location.replace("./comingsoon/comingsoon.html");
+const countDate = new Date('May 15, 2021 19:00:00').getTime();
+const now = new Date().getTime();
+const gap = countDate - now;
+
+if (gap > 0) {
+    window.location.replace("./comingsoon/comingsoon.html");
+}
